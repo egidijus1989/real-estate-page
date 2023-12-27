8157f09e817f684e2aa069324cf901aa37179588
egidijus <145454567+egidijus1989@users.noreply.github.com>
2023-12-27 19:06:50 +0200

    first commmit

--- api/index.js
+++ api/index.js
@@ -1,17 +1,38 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import { error } from "console";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-  console.log("Conected to mongodb")
-}).catch((err) =>{
-  console.log(err)
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(300, () => {
+app.use(express.json());
+
+app.listen(3000, () => {
   console.log("Server is running on 3000!!!");
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = error.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
