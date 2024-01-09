529aadfaccd83fdf9f957c6a804565441efbe981
egidijus <145454567+egidijus1989@users.noreply.github.com>
2024-01-09 18:15:58 +0200

    finish

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Ege nekilnojamo turto agentura
+      </h1>
+      <p className="mb-4 text-slate-800">
+        Our mission is to help buyer and seller to find each other and make best
+        deal
+      </p>
+    </div>
+  );
 }
